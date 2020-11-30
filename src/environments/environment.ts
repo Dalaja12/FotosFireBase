7724954a5a3a4a4fecb8b96d9eaac44ccf3fa1f7
juan-12286 <72234979+juan-12286@users.noreply.github.com>
2020-11-29 19:41:09 -0600

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+      apiKey: "AIzaSyDN6LlGkcBCYNuVp_MWpmP57YvaMWvN8ho",
+      authDomain: "fotospersonales-990fc.firebaseapp.com",
+      databaseURL: "https://fotospersonales-990fc.firebaseio.com",
+      projectId: "fotospersonales-990fc",
+      storageBucket: "fotospersonales-990fc.appspot.com",
+      messagingSenderId: "221911850002"
+  }
+};
